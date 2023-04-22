059b2140977f1ec53c68c6211d7990232bc70b60
tejasbbb <bharadwajtejas6@gmail.com>
2023-04-22 15:57:04 -0700

    authenticate feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Login from "./Components/Signup/Login";
 
 const App = () => {
-  return <div className="app">App Is Running</div>;
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
 };
 
 export default App;
